193d2114f38821808b6b40b447430f6f64235d7c
Ivan201352 <72215396+Ivan201352@users.noreply.github.com>
2022-06-23 12:04:17 +0300

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+SECRET_KEY = 'fbe6b0f9b97deadac8a8315c2c7d7421dc3d7a62f68263eac40fc150d60c65f7'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1697_exam:Ivan201-352@std-mysql.ist.mospolytech.ru/std_1697_exam'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
